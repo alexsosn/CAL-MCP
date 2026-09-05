8fa9a22ae1247279171e126441d20be1c1504db0
Oleksandr (Alex) Sosnovshchenko <alex.sosn88@gmail.com>
2026-09-05 23:14:37 +0300

    test: add returned source length regression

--- tests/test_external_citation_review_regressions.py
+++ tests/test_external_citation_review_regressions.py
@@ -9,15 +9,14 @@ from cal_mcp.external_citations import (
 )
 
 
-def test_returned_source_format_control_fails_closed() -> None:
-    abbreviation = "A\u200dB"
+def _source_response(abbreviation: str) -> CalResponse:
     body = (
         "<html><body><h1>Texts With Citations, No Full Text Yet</h1>"
         '<div class="cit-row"><a class="cit-abbrev" '
         f'href="/displaycits.abbrev.php?abbrev={abbreviation}">{abbreviation}</a>'
         "</div></body></html>"
     )
-    response = CalResponse(
+    return CalResponse(
         status_code=200,
         url="https://cal.huc.edu/display.notext.abbrevs.php?dial=6&dial1=6",
         body=body.encode("utf-8"),
@@ -25,5 +24,12 @@ def test_returned_source_format_control_fails_closed() -> None:
         retrieved_at=datetime(2026, 9, 5, 19, 50, tzinfo=UTC),
     )
 
+
+def test_returned_source_format_control_fails_closed() -> None:
     with pytest.raises(ExternalCitationParseError):
-        parse_external_citation_sources_page(response)
+        parse_external_citation_sources_page(_source_response("A\u200dB"))
+
+
+def test_returned_source_abbreviation_over_public_limit_fails_closed() -> None:
+    with pytest.raises(ExternalCitationParseError):
+        parse_external_citation_sources_page(_source_response("A" * 129))
