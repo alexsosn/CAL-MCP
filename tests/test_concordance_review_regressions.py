7bde2d2d72b25afb2f58a57d024abcad0d9a86bb
Oleksandr (Alex) Sosnovshchenko <alex.sosn88@gmail.com>
2026-09-05 12:11:59 +0300

    Add exact endpoint identity review regressions

--- tests/test_concordance_review_regressions.py
+++ tests/test_concordance_review_regressions.py
@@ -111,3 +111,66 @@ def test_dialect_selector_rejects_foreign_origin_target_form() -> None:
 
     with pytest.raises(ConcordanceParseError):
         parse_kwic_dialect_options(response, lemma_key=")ryk#2 A")
+
+
+def test_text_concordance_rejects_same_origin_endpoint_name_lookalike() -> None:
+    response = _response(
+        """<html><body>
+<div>Frequencies of lemmas in text 13250</div>
+<table><tr>
+<td>1:</td>
+<td><a href="/evilshowKWIC.php?lemma=mlk+N&amp;charset=S&amp;texts=13250">mlk N</a></td>
+<td>: king</td>
+</tr></table>
+</body></html>""",
+        "https://cal.huc.edu/newconcord.php?text=13250&cset=S",
+    )
+
+    with pytest.raises(ConcordanceParseError):
+        parse_text_concordance_page(
+            response,
+            requested_text_id="13250",
+            requested_charset="S",
+        )
+
+
+def test_kwic_rejects_same_origin_endpoint_name_lookalike() -> None:
+    response = _response(
+        """<html><body>
+<div>Looking for mlk N in dialect 13250</div>
+<div>13250:</div>
+<table><tr>
+<td>left context</td>
+<td><a href="/evilget_a_kwicchapter.php?file=13250&amp;sub=&amp;cset=R&amp;target=1325003">1325003</a></td>
+<td>right context</td>
+</tr></table>
+<div>total examples: 1</div>
+</body></html>""",
+        "https://cal.huc.edu/showdialectKWIC.php",
+    )
+
+    with pytest.raises(ConcordanceParseError):
+        parse_kwic_result(
+            response,
+            lemma_key="mlk N",
+            scope_kind=KwicScopeKind.TEXTS,
+            scope_ids=("13250",),
+        )
+
+
+def test_dialect_selector_rejects_same_origin_endpoint_name_lookalike() -> None:
+    response = _response(
+        """<html><body>
+<form action="/evilshow1dialectKWIC.php" method="get">
+<input type="hidden" name="lemma" value=")ryk#2">
+<input type="hidden" name="pos" value="A">
+<select name="texts">
+<option value="3">Biblical Aramaic</option>
+</select>
+</form>
+</body></html>""",
+        "https://cal.huc.edu/dKWIC.php?lemma=%29ryk%232+A",
+    )
+
+    with pytest.raises(ConcordanceParseError):
+        parse_kwic_dialect_options(response, lemma_key=")ryk#2 A")
